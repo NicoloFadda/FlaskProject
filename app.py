73d10b51535a77f5a422590259f380325ffc30aa
Nicolò Fadda <nicolo.fadda@CPT.local>
2024-09-16 10:40:09 +0200

    login e signup funzionanti

--- app.py
+++ app.py
@@ -1,10 +1,105 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+from models.conn import db
+from models.model import *
+
+from routes.auth import auth as bp_auth
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_hello_adm:Admin$00@localhost/flask_hello'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#L'opzione SQLALCHEMY_TRACK_MODIFICATIONS è una configurazione per Flask-SQLAlchemy che determina se SQLAlchemy deve o meno tenere traccia delle modifiche sugli oggetti per ogni sessione.
+db.init_app(app)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+app.register_blueprint(bp_auth, url_prefix='/auth')
+
+migrate = Migrate(app, db)
+
+
+# flask_login user loader block
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+# flask_login user loader block
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.execute(stmt).scalar_one_or_none()
+    
+    # return User.query.get(int(user_id))   # legacy
+    
+    return user
+
+# @app.route('/')
+# def home2():
+#     return "Hello, Flask!"
+
+# @app.route('/<nome>')
+# def home(nome):
+#     return render_template("hello.html", nome=nome)
+
+# @app.route('/value/<int:value>')
+# def value(value: int):
+#     return f'value: {10 + value}'
+
+# #nome "/power" deve essere come il nome della funzione
+# @app.route('/power/<int:value>')
+# def power(value: int):
+#     return f'value: {value * value}'
+
+@app.route('/createuser', methods=['POST'])
+def create_user():
+    values = request.json
+    username = values['username']
+    email = values['email']
+    password = values['password']
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return f"utente {username} creato con successo."
+
+
+@app.route('/testuser')
+def test_user():
+    # Creazione di un nuovo utente con una password criptata
+    user = User(username='testuser', email='test@example.com')
+    user.set_password('mysecretpassword')
+
+    # Aggiunta dell'utente al database
+    db.session.add(user)
+    db.session.commit()
+
+    # Verifica della password
+    if user.check_password('mysecretpassword'):
+        return "Password corretta!"
+    else:
+        return "Password errata!"
+        
+# @app.route('/createuser')
+# # def create_user(username, email, password):
+# def create_user():
+
+#     username = "test"
+#     email = "sus@sus.sus"
+#     password = "password"
+#     user = User(username=username, email=email)
+#     user.set_password(password)  # Imposta la password criptata
+#     db.session.add(user)  # equivalente a INSERT
+#     db.session.commit()
+#     return f"Utente {username} creato con successo."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
---
+++ routes/auth.py
@@ -0,0 +1,81 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import flash
+from flask import current_app   
+from flask_login import login_user  
+from flask_login import logout_user
+
+from models.conn import db
+from models.model import *
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    # shows the login form page
+    return render_template('auth/login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    # manages the login form post request
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or not user.check_password(password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('auth.profile'))    
+
+def logout():
+    return 'Logout'
+
+def profile():
+    return 'profile'
+
+@auth.route('/signup')
+def signup():
+    # shows the login form page
+    return render_template('auth/signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # signup input validation and logic
+    #TODO verify password strenght
+    username = request.form["username"] #as an alternative use request.form.get("username")
+    email = request.form["email"]    
+    password = request.form["password"]
+
+    if not username:
+        flash('Invalid username')
+        return redirect(url_for('auth.signup'))
+    if not email:
+        flash('Invalid email')
+        return redirect(url_for('auth.signup'))
+    if not password:
+        flash('Invalid password')
+        return redirect(url_for('auth.signup'))                
+    
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+    if user: 
+        # if a user is found, we want to redirect back to signup page so user can try again
+        # display some kind of error
+        flash('User with this email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    user = User(username=username, email=email)
+    user.set_password(password)  # Imposta la password criptata
+    db.session.add(user)  # equivalente a INSERT
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
